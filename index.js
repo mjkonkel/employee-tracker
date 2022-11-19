3b6d145c928ca2a9fad20f74a036ea4ddc9f95ef
mjkonkel <mitchell.konkel@gmail.com>
2022-11-19 15:25:26 -0600

    set up sql

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// require('dotenv').config();
-// process.env.DB_NAME
-
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-// questions
-const menuQuestion = [
+require('dotenv').config();
+require('console.table')
+
+const db = mysql.createConnection(
     {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'menuQuestion',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-        // add quit choice
-    }
-]
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the company_db database.`)
+  );
 
-// Add Department //
+// ----Add Department---- //
 const addDepartmentQuestion = [
     {
         type: 'input',
@@ -31,9 +31,9 @@ function addDepartment() {
         })
         .then(() => askMenu())
 }
-// Add Department //
+// ----Add Department---- //
 
-// Add Role //
+// ----Add Role---- //
 const addRoleQuestion = [
     {
         type: 'input',
@@ -46,21 +46,36 @@ const addRoleQuestion = [
         name: 'roleSalary'
     },
     {
-        type: 'input',
+        type: 'list',
         message: 'Which department does the role belong to?',
-        name: 'roleDepartment'
+        name: 'roleDepartment',
+        choices: ['Engineering', 'Finance', 'Legal', 'Sales']
     }
 ]
 
 function addRole() {
     inquirer
-    .prompt(addRoleQuestion)
-    .then((data) => {
-        console.log('Added ' + data.roleName + ' to the database')
-    })
-    .then(() => askMenu())
+        .prompt(addRoleQuestion)
+        .then((data) => {
+            console.log('Added ' + data.roleName + ' to the database')
+        })
+        .then(() => askMenu())
 }
-// Add Role //
+// ----Add Role---- //
+
+
+const addEmployeeQuestion =[
+    {
+        type: 'input',
+        message: 'What is the employee\'s first name?',
+        name: 'firstName'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s last name?',
+        name: 'lastName'
+    },
+]
 
 // add employee
 // What is the employee's first name?
@@ -68,20 +83,41 @@ function addRole() {
 // What is the employee's role?
 // Who is the employee's manager? (with none option)
 // log added 'employee' to the database
+function addEmployee() {
+    inquirer
+    .prompt(addEmployeeQuestion)
+}
 
 // update employee role
 // Which employee's role do you want to update?
 // Which role do you want to assign to the selected employee?
 // log updated employee's role
 
+// menu question
+const menuQuestion = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menuQuestion',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+    }
+]
+
+function viewAllEmployees() {
+    return db.promise().query('SELECT * FROM employee')
+}
 
 function askMenu() {
     inquirer
         .prompt(menuQuestion)
         .then((data) => {
             if (data.menuQuestion === 'View All Employees') {
-                console.log('View All Employees')
-                // viewAllEmployees()
+                // console.log('View All Employees')
+                viewAllEmployees().then((employeeData) => {
+                    console.table(employeeData[0])
+                    // console.log(employeeData)
+                    askMenu();
+                })
             }
             if (data.menuQuestion === 'Add Employee') {
                 console.log('Add Employee')
@@ -96,7 +132,7 @@ function askMenu() {
                 // viewAllRoles()
             }
             if (data.menuQuestion === 'Add Role') {
-                console.log('Add Role')
+                // console.log('Add Role')
                 addRole()
             }
             if (data.menuQuestion === 'View All Departments') {
@@ -104,16 +140,13 @@ function askMenu() {
                 // viewAllDepartments()
             }
             if (data.menuQuestion === 'Add Department') {
-                console.log('Add Department')
+                // console.log('Add Department')
                 addDepartment()
             }
-        })//.then(() => askMenu())
-
+            if (data.menuQuestion === 'Quit') {
+                askMenu()
+            }
+        })
 }
 
-
-function init() {
-    askMenu()
-}
-
-init()
+askMenu()
